98f9efb8e600b098049a06db5b9bad3bfd84e2a2
Herb Stahl <ghstahl@gmail.com>
2019-07-24 16:03:42 -0700

    added testserver fixture

---
+++ src/TestServerFixture/TestDefaultHttpClientFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using GraphQLPlay.IdentityModelExtras;
+using Microsoft.AspNetCore.TestHost;
+
+namespace TestServerFixture
+{
+    public class TestDefaultHttpClientFactory : IDefaultHttpClientFactory
+    {
+        public HttpMessageHandler HttpMessageHandler { get; set; }
+        public HttpClient HttpClient { get; set; }
+    }
+}
