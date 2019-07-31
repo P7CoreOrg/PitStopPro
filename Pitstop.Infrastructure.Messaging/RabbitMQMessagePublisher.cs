e99a7c5fd46a576447ed53dd4386f5b6b2c17c6d
Herb Stahl <ghstahl@gmail.com>
2019-07-30 20:12:58 -0700

    options for RabbitMQMessagePublisher

--- Pitstop.Infrastructure.Messaging/RabbitMQMessagePublisher.cs
+++ Pitstop.Infrastructure.Messaging/RabbitMQMessagePublisher.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Polly;
 using RabbitMQ.Client;
 using System;
@@ -14,19 +15,15 @@ namespace Pitstop.Infrastructure.Messaging
     /// </summary>
     public class RabbitMQMessagePublisher : IMessagePublisher
     {
-        private readonly ILogger<RabbitMQMessageHandler> _logger;
-        private readonly string _host;
-        private readonly string _username;
-        private readonly string _password;
-        private readonly string _exchange;
+        private readonly ILogger<RabbitMQMessagePublisher> _logger;
+        private readonly InfrastructureMessagingOptions _options;
 
-        public RabbitMQMessagePublisher(ILogger<RabbitMQMessageHandler> logger, string host, string username, string password, string exchange)
+        public RabbitMQMessagePublisher(
+            ILogger<RabbitMQMessagePublisher> logger,
+            IOptions<InfrastructureMessagingOptions> options)
         {
             _logger = logger;
-            _host = host;
-            _username = username;
-            _password = password;
-            _exchange = exchange;
+            _options = options.Value;
         }
 
         /// <summary>
@@ -43,17 +40,19 @@ namespace Pitstop.Infrastructure.Messaging
                     .WaitAndRetry(9, r => TimeSpan.FromSeconds(5), (ex, ts) => { _logger.LogError("Error connecting to RabbitMQ. Retrying in 5 sec."); })
                     .Execute(() =>
                     {
-                        var factory = new ConnectionFactory() { HostName = _host, UserName = _username, Password = _password };
+                        var factory = new ConnectionFactory() { HostName = _options.Host,
+                            UserName = _options.Username, Password = _options.Password
+                        };
                         using (var connection = factory.CreateConnection())
                         {
                             using (var model = connection.CreateModel())
                             {
-                                model.ExchangeDeclare(_exchange, "fanout", durable: true, autoDelete: false);
+                                model.ExchangeDeclare(_options.Exchange, "fanout", durable: true, autoDelete: false);
                                 string data = MessageSerializer.Serialize(message);
                                 var body = Encoding.UTF8.GetBytes(data);
                                 IBasicProperties properties = model.CreateBasicProperties();
                                 properties.Headers = new Dictionary<string, object> { { "MessageType", messageType } };
-                                model.BasicPublish(_exchange, routingKey, properties, body);
+                                model.BasicPublish(_options.Exchange, routingKey, properties, body);
                             }
                         }
                     }));
