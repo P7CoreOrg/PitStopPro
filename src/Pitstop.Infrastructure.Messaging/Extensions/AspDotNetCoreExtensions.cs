d55915885654234592dfa90241865bae4a35a243
Herb Stahl <ghstahl@gmail.com>
2019-07-31 08:50:18 -0700

    moved project

---
+++ src/Pitstop.Infrastructure.Messaging/Extensions/AspDotNetCoreExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pitstop.Infrastructure.Messaging.Extensions
+{
+   
+    public static class AspDotNetCoreExtensions
+    {
+
+        public static void AddInfrastructureMessaging(this IServiceCollection services,
+            Action<InfrastructureMessagingOptions> setupAction)
+        {
+            services.Configure(setupAction);
+            services.AddTransient<IMessagePublisher, RabbitMQMessagePublisher>();
+            services.AddTransient<IMessageHandler, RabbitMQMessageHandler>();
+        }
+    }
+}
